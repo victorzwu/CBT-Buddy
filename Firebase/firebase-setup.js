bbcb763f36c2d5b3aedf5954494d5aaaf3a61d6f
Victor Wu <victor.wu08@gmail.com>
2023-03-23 11:39:45 -0700

    add firebase setup file

---
+++ Firebase/firebase-setup.js
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import React from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} from "@env";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+};
+
+// Initialize Firebase
+const myApp = initializeApp(firebaseConfig);
+export const firestore = getFirestore(myApp);
+
+//create authentication token
+export const auth = initializeAuth(myApp, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+//export storage
+export const storage = getStorage(myApp);
