28978693c7c2a284da57f3b357caaf91b95a6fc7
Victor Wu <victor.wu08@gmail.com>
2023-03-30 12:34:31 -0700

    setting up 3rd party API call

--- CityOfVancouver/cityofvancouver-setup.js
+++ CityOfVancouver/cityofvancouver-setup.js
@@ -2,3 +2,15 @@ import {
     cityVanApiKey
   } from "@env";
 
+  fetch('https://opendata.vancouver.ca/api/v2/', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: cityVanApiKey,
+    },
+    body: JSON.stringify({
+      firstParam: 'yourValue',
+      secondParam: 'yourOtherValue',
+    }),
+  });
