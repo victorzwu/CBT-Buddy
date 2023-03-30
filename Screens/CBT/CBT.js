3956a6d5cd504092b6f6ec0be9d7376c2c1c315f
Victor Wu <victor.wu08@gmail.com>
2023-03-30 13:39:05 -0700

    create state variable for entries, change CBT All Entries screen to accept entries prop

--- Screens/CBT/CBT.js
+++ Screens/CBT/CBT.js
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CBTAllEntries from "./CBTAllEntries";
 import CBTDetails from "./CBTDetails";
@@ -10,34 +10,68 @@ import CBTReview from "./CBTReview";
 import { deleteFromCBT, editFromCBT } from "../../Firebase/fireStoreHelper";
 
 export default function CBT() {
+  const [entries, setEntries] = useState([]);
 
   const Stack = createNativeStackNavigator();
 
-  function addEntry(situation, distortion, reframe, date)
-  {
-    let entry = { situation: situation, distortion: distortion, reframe: reframe, date: date}
+  function addEntry(situation, distortion, reframe, date) {
+    let entry = {
+      situation: situation,
+      distortion: distortion,
+      reframe: reframe,
+      date: date,
+    };
     writeToCBT(entry);
   }
 
-  function editEntry(id, situation, distortion, reframe, date)
-  {
-    let entry = { situation: situation, distortion: distortion, reframe: reframe, date: date}
+  function editEntry(id, situation, distortion, reframe, date) {
+    let entry = {
+      situation: situation,
+      distortion: distortion,
+      reframe: reframe,
+      date: date,
+    };
     editFromCBT(id, entry);
   }
 
-  function deleteEntry(id)
-  {
+  function deleteEntry(id) {
     deleteFromCBT(id);
   }
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name="CBT" component={CBTAllEntries} options={{ headerShown: false }} />
-      <Stack.Screen name="Describe the Situation" component={CBTDescribe} options={{ headerShown: false }} />
-      <Stack.Screen name="Cognitive Distortions" component={CBTCognitiveDistortions} options={{ headerShown: false }} />
-      <Stack.Screen name="Challenge the Thought" component={CBTReframe} options={{ headerShown: false }} />
-      <Stack.Screen name="Review" component={CBTReview} options={{ headerShown: false }} />
-      <Stack.Screen name="Details" component={CBTDetails} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="CBT"
+        component={CBTAllEntries}
+        options={{ headerShown: false }}
+      >
+        {(props) => <CBTAllEntries {...props} entries={entries} />}
+      </Stack.Screen>
+      <Stack.Screen
+        name="Describe the Situation"
+        component={CBTDescribe}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Cognitive Distortions"
+        component={CBTCognitiveDistortions}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Challenge the Thought"
+        component={CBTReframe}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Review"
+        component={CBTReview}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={CBTDetails}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
