2a8f5e1f1c132b59e2dbebab4c6dffc096c9c6e1
Victor Wu <victor.wu08@gmail.com>
2023-03-30 18:25:08 -0700

    add SignUp screen

--- Screens/Authentication/SignUp.js
+++ Screens/Authentication/SignUp.js
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TextInput } from "react-native";
+import React, { useState } from "react";
+import { auth } from "../Firebase/firebase-setup";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-export default function SignUp() {
+export default function SignUp({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const login = () => {
+    navigation.replace("Login");
+  };
+
+  const signUp = async () => {
+    if (password !== confirmPassword) {
+      Alert.alert("The passwords don't match");
+    }
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.log("Auth error ", err);
+    }
+  };
+
   return (
     <View>
-      <Text>SignUp</Text>
+      <Text>Email Address</Text>
+      <TextInput
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        placeholder="Enter Email"
+      />
+      <Text>Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Enter Password"
+      />
+      <Text>Confirm password</Text>
+      <TextInput
+        value={confirmPassword}
+        onChangeText={(text) => setConfirmPassword(text)}
+        placeholder="Confirm Password"
+      />
+      <Button title="Register" onPress={signUp} />
+      <Button title="Already Registered? Login" onPress={login} />
     </View>
-  )
+  );
 }
