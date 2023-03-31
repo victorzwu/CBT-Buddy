d4bca1a1a003e43940bf1351b14cd16d31ea0473
Yiwei Tao <tao.yiw@northeastern.edu>
2023-03-30 22:41:04 -0700

    test function

---
+++ Screens/CBT/CBTScreen1.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CBTScreen1() {
+  return (
+    <View>
+      <Text>CBTScreen1</Text>
+    </View>
+  )
+}
