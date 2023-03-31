63aa243d81b3bfd57b5ceea476944db06dc1ddb3
Victor Wu <victor.wu08@gmail.com>
2023-03-30 18:46:49 -0700

    add fetch API to resources

--- Screens/Resources/Resources.js
+++ Screens/Resources/Resources.js
@@ -1,13 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ResourcesScreen1 from './ResourcesScreen1';
+import { View, Text } from "react-native";
+import React from "react";
+import { useEffect } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ResourcesScreen1 from "./ResourcesScreen1";
+import {
+  cityVanApiKey
+} from "@env";
+
 
 export default function Resources() {
   const Stack = createNativeStackNavigator();
+
+  useEffect(() => {
+    const fetchResources = async () => {
+      fetch('https://opendata.vancouver.ca/api/v2/', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: cityVanApiKey,
+        },
+        body: JSON.stringify({
+          firstParam: 'yourValue',
+          secondParam: 'yourOtherValue',
+        }),
+      });
+    };
+    fetchResources();
+  });
+
+  // , {
+  //   method: 'GET',
+  //   headers: {
+  //     Accept: 'application/json',
+  //     'Content-Type': 'application/json',
+  //     Authorization: cityVanApiKey,
+  //   },
+  //   body: JSON.stringify({
+  //     firstParam: 'yourValue',
+  //     secondParam: 'yourOtherValue',
+  //   });
+  // }
+  // ));
   return (
     <Stack.Navigator>
-      <Stack.Screen name = "Relaxation Screen 1" component = {ResourcesScreen1}/>
+      <Stack.Screen name="Relaxation Screen 1" component={ResourcesScreen1} />
     </Stack.Navigator>
-  )
+  );
 }
