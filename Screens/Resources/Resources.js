aaf7d826c08b2b15f0b670facf91f807931322fa
Victor Wu <victor.wu08@gmail.com>
2023-03-31 15:32:20 -0700

    trying out fetch

--- Screens/Resources/Resources.js
+++ Screens/Resources/Resources.js
@@ -3,28 +3,30 @@ import React from "react";
 import { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ResourcesScreen1 from "./ResourcesScreen1";
-import {
-  cityVanApiKey
-} from "@env";
-
+import { cityVanApiKey } from "@env";
 
 export default function Resources() {
   const Stack = createNativeStackNavigator();
 
   useEffect(() => {
     const fetchResources = async () => {
-      fetch('https://opendata.vancouver.ca/api/v2/', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: cityVanApiKey,
-        },
-        body: JSON.stringify({
-          firstParam: 'yourValue',
-          secondParam: 'yourOtherValue',
-        }),
-      });
+      try {
+        const response = await fetch(
+          "https://opendata.vancouver.ca/api/v2/catalog/datasets/business-licences",
+          {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: cityVanApiKey,
+            },
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+      } catch (err) {
+        console.log("api error: ", err);
+      }
     };
     fetchResources();
   });
