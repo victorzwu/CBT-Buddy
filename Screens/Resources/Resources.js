2c01cb01654d1a73556dd4150a63f6d987837f5a
Victor Wu <victor.wu08@gmail.com>
2023-03-31 16:13:01 -0700

    fetched resources properly. fixing problem with object rendering

--- Screens/Resources/Resources.js
+++ Screens/Resources/Resources.js
@@ -3,40 +3,14 @@ import React from "react";
 import { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ResourcesScreen1 from "./ResourcesScreen1";
-import { cityVanApiKey } from "@env";
+
 
 export default function Resources() {
   const Stack = createNativeStackNavigator();
 
-  useEffect(() => {
-    const fetchResources = async () => {
-      try {
-        const response = await fetch(
-          "https://opendata.vancouver.ca/api/v2/catalog/datasets/business-licences/records?limit=10&offset=0&refine=status%3AIssued&refine=businesssubtype%3ACounselling&timezone=UTC",
-          {
-            method: "GET",
-            headers: {
-              Accept: "application/json",
-              // "Content-Type": "application/json",
-              Authorization: cityVanApiKey,
-            },
-          }
-        );
-        const data = await response.json();
-        const test = data.records.map((business) => {
-          return { name: business.record.fields.businessname, city: business.record.fields.city, localarea: business.record.fields.localarea };
-        });
-
-        console.log(test);
-      } catch (err) {
-        console.log("api error: ", err);
-      }
-    };
-    fetchResources();
-  }, []);
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Relaxation Screen 1" component={ResourcesScreen1} />
+      <Stack.Screen name="Resources Screen 1" component={ResourcesScreen1} />
     </Stack.Navigator>
   );
 }
--- Screens/Resources/ResourcesScreen1.js
+++ Screens/Resources/ResourcesScreen1.js
@@ -1,8 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useEffect } from 'react';
+import { View, Text } from "react-native";
+import React from "react";
+import { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import { cityVanApiKey } from "@env";
 
 export default function ResourcesScreen1() {
+  const [resources, setResources] = useState([]);
 
   useEffect(() => {
     const fetchResources = async () => {
@@ -19,11 +22,16 @@ export default function ResourcesScreen1() {
           }
         );
         const data = await response.json();
-        const test = data.records.map((business) => {
-          return { name: business.record.fields.businessname, city: business.record.fields.city, localarea: business.record.fields.localarea };
+        const businesses = data.records.map((business) => {
+          return (
+            {
+            name: business.record.fields.businessname,
+            city: business.record.fields.city,
+            localarea: business.record.fields.localarea,
+          });
         });
 
-        console.log(test);
+        await setResources(businesses);
       } catch (err) {
         console.log("api error: ", err);
       }
@@ -33,7 +41,15 @@ export default function ResourcesScreen1() {
 
   return (
     <View>
-      <Text>ResourcesScreen1</Text>
+      {resources && (
+        <FlatList
+          data={resources}
+          renderItem={(resource) => {
+            return <Text>{resource.localarea}</Text>;
+          }}
+        />
+      )}
+      <Text>Hi</Text>
     </View>
-  )
+  );
 }
