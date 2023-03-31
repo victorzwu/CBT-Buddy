0b22ce27f608646873d3e062713bb90909990f67
Yiwei Tao <tao.yiw@northeastern.edu>
2023-03-30 22:36:15 -0700

    add map positioning and photo function

---
+++ Screens/Journal/AddPhotoAndLocation.js
@@ -0,0 +1,202 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import React, { useState, useEffect } from "react";
+import { storage } from "../../Firebase/firebase-setup";
+import { COLORS } from "../../color";
+import { update } from "../../Firebase/firestore";
+import { AntDesign, FontAwesome5, FontAwesome } from "@expo/vector-icons";
+import { ref, uploadBytes } from "firebase/storage";
+import moment from "moment";
+
+export default function Com({ formData, setFormData, navigation, getData }) {
+  const [image, setImage] = useState(null);
+  const [cameraImage, setCameraImage] = useState(null);
+  const getImageBlob = async (uri) => {
+    try {
+      const response = await fetch(uri);
+      const blob = await response.blob();
+      return blob;
+    } catch (err) {
+      console.log("fetch image ", err);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } = await ImagePicker.requestCameraPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    let result = await ImagePicker.launchCameraAsync();
+    if (!result.cancelled) {
+      // setImage(result.uri);
+      let uri = result.uri;
+      let imgBlob = await getImageBlob(uri);
+      let uuid = moment().valueOf();
+      const storageRef = ref(storage, `${uuid}_img`);
+      uploadBytes(storageRef, imgBlob)
+        .then((snapshot) => {
+          console.log("Uploaded a blob!");
+          setFormData({
+            ...formData,
+            photo: `${uuid}_img`,
+          });
+          setCameraImage(uri);
+          if (formData.id) {
+            update(formData.id, {
+              photo: `${uuid}_img`,
+            });
+            alert("Edit Success!");
+            getData();
+            navigation.goBack();
+          }
+        })
+        .catch((err) => {
+          console.log("err = ", err);
+        });
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      allowsMultipleSelection: false,
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      let uri = result.uri;
+      let imgBlob = await getImageBlob(uri);
+      let uuid = moment().valueOf();
+      const storageRef = ref(storage, `${uuid}_img`);
+      uploadBytes(storageRef, imgBlob)
+        .then((snapshot) => {
+          console.log("Uploaded a blob!");
+          setFormData({
+            ...formData,
+            photo: `${uuid}_img`,
+          });
+          setImage(uri);
+          if (formData.id) {
+            update(formData.id, {
+              photo: `${uuid}_img`,
+            });
+            alert("Edit Success!");
+            getData();
+            navigation.goBack();
+          }
+        })
+        .catch((err) => {
+          console.log("err = ", err);
+        });
+    }
+  };
+
+  useEffect(() => {
+    // get image
+    // getDownloadURL(ref(storage, "some-child")).then((url) => {
+    //   console.log("url = ", url);
+    //   setImage(url);
+    // });
+  }, []);
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.tit}>Photo And Location</Text>
+      <View style={styles.itemBox}>
+        <Text style={styles.itemTip}>Tap the photo</Text>
+        <TouchableOpacity onPress={pickImage} style={styles.item}>
+          {image ? (
+            <Image source={{ uri: image }} style={styles.image} />
+          ) : (
+            <FontAwesome name="photo" size={80} color={COLORS.primary} />
+          )}
+        </TouchableOpacity>
+      </View>
+      <View style={styles.itemBox}>
+        <Text style={styles.itemTip}>Tap the camera</Text>
+        <TouchableOpacity onPress={takePicture} style={styles.item}>
+          {cameraImage ? (
+            <Image source={{ uri: cameraImage }} style={styles.image} />
+          ) : (
+            <AntDesign name="camera" size={100} color={COLORS.primary} />
+          )}
+        </TouchableOpacity>
+      </View>
+      <View style={styles.itemBox}>
+        <Text style={styles.itemTip}>Tap the map</Text>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate({
+              name: "Map",
+            });
+          }}
+          style={styles.itemMap}
+        >
+          <FontAwesome5
+            name="map-marker-alt"
+            size={100}
+            color={COLORS.yellow}
+          />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tit: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    paddingTop: 30,
+  },
+  itemBox: {
+    alignItems: "center",
+    marginTop: 30,
+  },
+  item: {
+    width: 120,
+    height: 120,
+    borderWidth: 4,
+    borderColor: COLORS.primary,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  itemTip: {
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  itemMap: {
+    width: 120,
+    height: 120,
+    borderWidth: 4,
+    borderColor: COLORS.yellow,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
---
+++ Screens/Journal/Map.js
@@ -0,0 +1,138 @@
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import Button from "../../Components/Button";
+import MapView from "react-native-maps";
+import { COLORS } from "../../color";
+import { update } from "../../Firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { firestore } from "../../Firebase/firebase-setup";
+import { addDoc, collection } from "firebase/firestore";
+// import { MAP_API_KEY } from "@env";
+const MAP_API_KEY="AIzaSyCB-LzTwbzYe4FNuj8VNoWQrS-PtJTQcPU"
+
+export default function Com({ formData, setFormData, navigation, getData }) {
+  const [address, setAddress] = useState(null);
+
+  useEffect(() => {
+
+  }, []);
+
+  const handleMapPress = async (coordinate) => {
+    const { latitude, longitude } = coordinate;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${MAP_API_KEY}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const address = data.results[0].formatted_address;
+      setAddress(address);
+      if (!formData.id) {
+        setFormData({
+          ...formData,
+          location: address,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={styles.box}>
+        <Text style={styles.tit}>Click on the map to select a location</Text>
+        <View style={styles.curAddressBox}>
+          <Text style={styles.curAddress}>
+            {address ? address : "no select"}
+          </Text>
+        </View>
+        <View style={styles.btnBox}>
+          <Button
+            onPress={async () => {
+              try {
+                if (formData.id) {
+                  update(formData.id, {
+                    location: address,
+                  });
+                  setFormData({
+                    ...formData,
+                    location: address,
+                  });
+                  alert("Edit Success!");
+                  getData();
+                  navigation.goBack();
+                } else {
+                  await addDoc(collection(firestore, "journals"), {
+                    ...formData,
+                  });
+                  alert("ADD Success!");
+                  getData();
+                  navigation.navigate({
+                    name: "Journal",
+                  });
+                }
+              } catch (e) {
+                console.error("Error adding document: ", e);
+              }
+            }}
+            title="Confirm"
+          />
+        </View>
+
+        <View style={styles.mapBox}>
+          <MapView
+            onPress={(e) => {
+              handleMapPress(e.nativeEvent.coordinate);
+            }}
+            style={styles.mapView}
+            initialRegion={{
+              latitude: 49.22796918780358,
+              longitude: -123.00663209296373,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+  box: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  tit: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    paddingVertical: 20,
+  },
+  mapBox: {
+    flex: 1,
+  },
+  mapView: {
+    flex: 1,
+  },
+  btnBox: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingBottom: 20,
+  },
+  curAddressBox: {
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  curAddress: {
+    color: COLORS.primary,
+    textAlign: "center",
+  },
+});
