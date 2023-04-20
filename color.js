3d36d7cedfa1af5353fbe318f9ed08cf7ead5653
Victor <victor.wu08@gmail.com>
2023-04-20 15:51:04 -0700

    change background image

--- color.js
+++ color.js
@@ -1,9 +1,4 @@
 export const COLORS = {
-  // firstColor: '#EDF2FB',
-  // secondColor: '#D7E3FC',
-  // thirdColor: '#C1D3FE',
-  // fourthColor: '#B6CCFE',
-  // fifthColor: '#ABC4FF',
   primary: '#87CBB9',
   second: '#bae7ff',
   white: 'white',
