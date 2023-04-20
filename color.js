3e4c2821186cee504cea173e814cdb51839880e5
Victor <victor.wu08@gmail.com>
2023-04-20 15:24:24 -0700

    change color back

--- color.js
+++ color.js
@@ -4,14 +4,12 @@ export const COLORS = {
   // thirdColor: '#C1D3FE',
   // fourthColor: '#B6CCFE',
   // fifthColor: '#ABC4FF',
-  primary: '#000B4F',
+  primary: '#87CBB9',
   second: '#bae7ff',
-  danger: '#fa541c',
   white: 'white',
   yellow: "#569DAA",
   textColor: '#577D86',
   background: "#87CBB9",
   grey: "#577D86",
   darksilver: "#569DAA"
-
 }
