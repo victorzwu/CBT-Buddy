da1248da7fa76a507c5b0ec460b72b8eaad5bd40
Yiwei Tao <tao.yiw@northeastern.edu>
2023-04-21 20:19:39 -0700

    merge

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 ## Iteration 3
 
+### Yiwei Tao
+
+Allow users to only be able to read their own entries in the database by using auth uid.
+
+<img src="./assets/readme/authuid.png" height="30%" width="30%" > 
+
+Remove Alerts from Delete and Add journal entry and add submit button to Add Entry page.
+
+<img src="./assets/readme/addButton.png" height="30%" width="30%" > 
+
+Update the 'Edit' page to allow changes to be made to specific entries.
+
+<img src="./assets/readme/edit1.png" height="30%" width="30%" > 
+
+Implement the ability to change avatars and store them in storage.
+
+<img src="./assets/readme/avatar.png" height="30%" width="30%" > 
+
 ### Victor
 
 Added Favorites Page to Resource with CRD operations
--- Screens/Profile/ProfileScreen.js
+++ Screens/Profile/ProfileScreen.js
@@ -1,5 +1,5 @@
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet, ActivityIndicator, Alert} from "react-native";
 import { auth, storage } from "../../Firebase/firebase-setup";
 import { signOut } from "firebase/auth";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
@@ -10,7 +10,8 @@ import { getTime } from "date-fns";
 
 export default function ProfileScreen() {
   const [avatar, setAvatar] = useState("");
-  
+  const [newAvatar, setNewAvatar] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchAvatar = async () => {
@@ -23,7 +24,7 @@ export default function ProfileScreen() {
     };
     fetchAvatar();
   }, []);
-
+  
   const openImagePickerAsync = async () => {
     try {
       const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -31,49 +32,62 @@ export default function ProfileScreen() {
         alert("Sorry, we need camera roll permissions to make this work!");
         return;
       }
-  
+
       let result = await ImagePicker.launchImageLibraryAsync({
         allowsEditing: true,
         aspect: [1, 1],
         quality: 0.5,
       });
-  
-      // console.log('Image Picker Result:', result);
-  
+
       if (!result.canceled && result.assets && result.assets[0]) {
         const asset = result.assets[0];
         console.log('Selected Asset:', asset);
-  
-        setAvatar(asset.uri);
+
+        setNewAvatar(asset.uri);
         const response = await fetch(asset.uri);
         const blob = await response.blob();
         const timestamp = getTime(new Date());
         const storageRef = ref(storage, `avatars/${auth.currentUser.uid}/${timestamp}`);
 
-        // const storageRef = ref(storage, `avatars/${auth.currentUser.uid}`);
+        setLoading(true);
         await uploadBytes(storageRef, blob);
+        const newAvatarUrl = await getDownloadURL(storageRef);
+        setAvatar(newAvatarUrl);
+        setNewAvatar(null);
+        setLoading(false);
       }
     } catch (error) {
       console.log('Error in openImagePickerAsync:', error);
     }
   };
-  
 
   const changeAvatar = () => {
-    openImagePickerAsync();
+    Alert.alert(
+      "Change Avatar",
+      "Are you sure you want to change your avatar?",
+      [
+        { text: "Cancel", style: "cancel" },
+        { text: "Yes", onPress: () => openImagePickerAsync() },
+      ],
+      { cancelable: false }
+    );
   };
-
   return (
     <View style={styles.container}>
       <View style={styles.avatarContainer}>
-        <TouchableOpacity onPress={changeAvatar}>
-          {avatar ? (
-            <Image source={{ uri: avatar }} style={styles.avatarImage} />
+        <TouchableOpacity onPress={changeAvatar} disabled={loading}>
+          {newAvatar ? (
+            <Image source={{ uri: newAvatar }} style={styles.avatarImage} />
           ) : (
-            <View style={styles.avatarPlaceholder}>
-              <Text style={styles.avatarText}>Photo</Text>
-            </View>
+            avatar ? (
+              <Image source={{ uri: avatar }} style={styles.avatarImage} />
+            ) : (
+              <View style={styles.avatarPlaceholder}>
+                <Text style={styles.avatarText}>Photo</Text>
+              </View>
+            )
           )}
+          {loading && <ActivityIndicator size="large" color={COLORS.primary} style={styles.loadingIndicator} />}
         </TouchableOpacity>
       </View>
       <Text>Email: {auth.currentUser.email}</Text>
@@ -89,7 +103,6 @@ export default function ProfileScreen() {
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -105,23 +118,33 @@ const styles = StyleSheet.create({
     borderColor: "black",
     overflow: "hidden",
     marginBottom: 20,
+    position: "relative",
   },
   avatarImage: {
     height: "100%",
     width: "100%",
-  },
-  avatarPlaceholder: {
+    },
+    avatarPlaceholder: {
     height: "100%",
     width: "100%",
     backgroundColor: "#EFEFEF",
     alignItems: "center",
     justifyContent: "center",
-  },
-  avatarText: {
+    },
+    avatarText: {
     fontSize: 16,
     fontWeight: "bold",
-  },
-  signOutButton: {
+    },
+    signOutButton: {
     marginTop: 20,
-  },
-});
+    },
+    loadingIndicator: {
+    position: "absolute",
+    zIndex: 1,
+    height: "100%",
+    width: "100%",
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    alignItems: "center",
+    justifyContent: "center",
+    },
+    });
